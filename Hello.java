f1aa5066c5de921efff7a5032b90b2b9635ea661
mldn <yxj9187@qq.com>
2017-10-06 15:56:53 +0800

    Change Hello.java in master branch

--- Hello.java
+++ Hello.java
@@ -1,9 +1,7 @@
+import java.util.Date
+
 public class Hello{
 	public static void main(String args[]){
-		System.out.println("Hello GIT !!!");
-		System.out.println("认真听课的孩子才是好孩子");
-		System.out.println("不认真听课的孩子不是好孩子");
-		System.out.println("世界你好");
-		System.out.println("111");
+		Date date = new Date();
 	}
 }
